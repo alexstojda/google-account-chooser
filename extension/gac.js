9b5069d9e0c1ca67408b64013221b73e054728d4
Jean-Martin Archer <jean-martin.archer@nesto.ca>
2024-06-05 10:51:06 -0700

    handle login flow

---
+++ extension/gac.js
@@ -0,0 +1,132 @@
+//
+// Copyright 2016-2020 - Sverrir A. Berg <sab@keilir.com>
+// See LICENSE file for more information.
+//
+
+const DEBUG = false;
+const DELAY_LOGIN_MILLISECONDS = 500;
+
+if (DEBUG) console.log("google_account.js - in like Flynn!");
+
+const gacGetDomain = () => {
+  try {
+    let domain = document.referrer.split('/')[2];
+    if (domain === undefined) {
+      return "test.html";
+    }
+    return domain;
+  } catch (err) {
+    if (DEBUG) console.log("Problem getting domain from referrer:", err.mesasge);
+    return "NONE";
+  }
+};
+
+const gacGetLoginElements = () => document.querySelectorAll('[data-identifier]');
+
+
+const gacPerformLogin = email => {
+  if (DEBUG) console.log("gacPerformLogin: %s", email);
+  let loginElements = gacGetLoginElements();
+
+
+  // Find and click if the account match given email
+  for (let i = 0; i < loginElements.length; i++) {
+    let el = loginElements[i];
+    let elEmail = el.getAttribute("data-identifier");
+    if (DEBUG) console.log("gacFoundEntry: %s", elEmail);
+    if (elEmail === email) {
+      if (DEBUG) console.log("clicking button for email:", email);
+      el.click();
+      return true;
+    }
+  }
+
+  if (DEBUG) console.log("gacPerformLogin: did not match any");
+  return false;
+};
+
+const gacClickHandler = (domain, el) => () => {
+  let email = el.getAttribute("data-identifier");
+  if (DEBUG) console.log("clickHandler: registering %s -> %s", domain, email);
+  chrome.runtime.sendMessage(
+    {
+      action: "setEmail",
+      email: email,
+      domain: domain
+    },
+    () => {
+      if (DEBUG) console.log("clickHandler: done registering %s -> %s", domain, email);
+    }
+  );
+};
+
+const gacStartup = () => {
+  let domain = gacGetDomain();
+  let loginElements = gacGetLoginElements();
+
+  // Register click handlers
+  loginElements.forEach(
+    el => el.addEventListener("click", gacClickHandler(domain, el), false)
+  );
+
+  // Auto click the first account if there is only one
+  if (loginElements.length == 1) {
+    setTimeout(() => {
+      let el = loginElements[0];
+      el.click();
+    }, DELAY_LOGIN_MILLISECONDS);
+    return;
+  }
+
+  // Try to see if we should log in automatically.
+  chrome.runtime.sendMessage(
+    {
+      action: "getEmail",
+      domain: domain
+    },
+    response => {
+      if (response !== undefined) {
+        if (response.email) {
+          setTimeout(gacPerformLogin, DELAY_LOGIN_MILLISECONDS, response.email);
+        }
+      }
+    }
+  );
+};
+
+const clickFn = (selector, text) => () => {
+  const nodes = document.querySelectorAll(selector);
+  const el = Array.from(nodes).find(span => span.textContent.trim() === text);
+  el.click()
+};
+
+
+const safelyAllowGCP = () => {
+  const spans = document.querySelectorAll('span');
+  if (Array.from(spans).find(span => span.textContent.trim() === 'Make sure you trust Google Cloud SDK')) {
+    Array.from(spans).find(span => span.textContent.trim() === 'Allow').click()
+  }
+};
+
+const main = () => {
+  gacStartup();
+
+  const map = {
+    "https://accounts.google.com/signout/chrome/landing?": clickFn('span', 'Sign in again'),
+    "https://accounts.google.com/signin/oauth/id?": clickFn('span', 'Continue'),
+    "https://accounts.google.com/v3/signin/challenge/pk/presend?": clickFn('span', 'Continue'),
+    "https://accounts.google.com/signin/oauth/consent?": safelyAllowGCP,
+    "https://accounts.google.com/v3/signin/confirmidentifier?": clickFn('span', 'Next'),
+    "https://app.strongdm.com/app/auth/native-continue?": _ => setTimeout(clickFn('div', 'Log in with Google'), 1000),
+  }
+
+  for (const [url, fn] of Object.entries(map)) {
+    if (document.location.href.startsWith(url)) {
+      setTimeout(fn, 100)
+      return
+    }
+  }
+};
+
+main()
+
--- extension/service_worker.js
+++ extension/service_worker.js
@@ -14,154 +14,106 @@ const DEBUG = false;
 let db = null;
 let tabState = {};
 
-function initDatabase() {
-    chrome.storage.sync.get(null, function (data) {
-        if (DEBUG) console.log("db init:", data);
-        db = data;
-    });
-}
+const initDatabase = () => {
+  chrome.storage.sync.get(null, data => {
+    if (DEBUG) console.log("db init:", data);
+    db = data;
+  });
+};
 
 initDatabase();
 
-function clearDatabase() {
-    chrome.storage.sync.clear();
-}
+const getEmail = domain => {
+  if (DEBUG) console.log("getEmail: %s -> %s", domain, db[domain]);
+  return db[domain];
+};
 
-function getDatabase() {
-    return db;
-}
-
-function setEmail(domain, email) {
-    if (getEmail(domain) === email) {
-        return;
+const setEmail = (domain, email) => {
+  if (getEmail(domain) === email) {
+    return;
+  }
+  let update = {};
+  update[domain] = email;
+  if (DEBUG) console.log("setEmail:", update);
+  chrome.storage.sync.set(
+    update,
+    () => {
+      if (chrome.runtime.lastError) {
+        console.log("setEmail: ERROR: %s", chrome.runtime.lastError)
+      } else {
+        if (DEBUG) console.log("setEmail: success");
+      }
     }
-    let update = {};
-    update[domain] = email;
-    if (DEBUG) console.log("setEmail:", update);
-    chrome.storage.sync.set(
-        update,
-        function() {
-            if (chrome.runtime.lastError) {
-                console.log("setEmail: ERROR: %s", chrome.runtime.lastError)
-            }
-            else {
-                if (DEBUG) console.log("setEmail: success");
-            }
-        }
-    );
-}
-
-function getEmail(domain) {
-    if (DEBUG) console.log("getEmail: %s -> %s", domain, db[domain]);
-    return db[domain];
-}
-
-function getTabData(tabId) {
-    if (tabId in tabState) {
-        return tabState[tabId];
-    }
-    else {
-        return null;
-    }
-}
-
-function setPageState(tabId, domain, email, noSets) {
-    if (DEBUG) console.log("setPageState: %s / %s [%d], %d", domain, email, tabId, noSets);
-    tabState[tabId] = {
-        domain: domain,
-        email: email
-    };
-    setEmail(domain, email);
+  );
+};
 
 
-    // make sure icon is set after page navigation
-    for (let i = 0; i < noSets; i++) {
-        setTimeout(function() {
-            setIconState(tabId, domain, email);
-        }, i * 500);
-    }
+const setPageState = (tabId, domain, email, noSets) => {
+  if (DEBUG) console.log("setPageState: %s / %s [%d], %d", domain, email, tabId, noSets);
+  tabState[tabId] = {
+    domain: domain,
+    email: email
+  };
+  setEmail(domain, email);
+};
 
-}
+const accountSelected = (tabId, domain, email) => {
+  if (DEBUG) console.log("accountSelected: %s / %s [%d]", domain, email, tabId);
+  let oldEmail = getEmail(domain);
 
-function accountSelected(tabId, domain, email) {
-    if (DEBUG) console.log("accountSelected: %s / %s [%d]", domain, email, tabId);
-    let oldEmail = getEmail(domain);
-
-    if (oldEmail === DISABLED) {
-        email = DISABLED;
-        if (DEBUG) console.log("Domain disabled: %s / %s [%d]", domain, email, tabId);
-    }
-    setPageState(tabId, domain, email, 10);
-}
-
-function setIconState(tabId, domain, email) {
-    let icon = "res/blue_icon.png";
-    if (email === DISABLED) {
-        icon = "res/red_icon.png";
-    }
-    else if (!email || email === EMPTY) {
-        icon = "res/gray_icon.png";
-    }
-    chrome.pageAction.setIcon({
-        tabId: tabId,
-        path : {
-            "38": icon
-        }
-    });
-    chrome.pageAction.show(tabId);
-}
+  if (oldEmail === DISABLED) {
+    email = DISABLED;
+    if (DEBUG) console.log("Domain disabled: %s / %s [%d]", domain, email, tabId);
+  }
+  setPageState(tabId, domain, email, 10);
+};
 
 //
 // Register global callback functions
 //
 
-chrome.storage.onChanged.addListener(function(changes, namespace) {
-    if (namespace === "sync") {
-        for (let key in changes) {
-            if (changes.hasOwnProperty(key)) {
-                let values = changes[key];
-                if (DEBUG) {
-                    console.log("values:", values);
-                    console.log("db change: %s -> %s (from %s)", key, values.newValue, values.oldValue);
-                }
-                if (values.hasOwnProperty("newValue")) {
-                    db[key] = values.newValue;
-                }
-                else {
-                    delete db[key];
-                }
-            }
+chrome.storage.onChanged.addListener((changes, namespace) => {
+  if (namespace === "sync") {
+    for (let key in changes) {
+      if (changes.hasOwnProperty(key)) {
+        let values = changes[key];
+        if (DEBUG) {
+          console.log("values:", values);
+          console.log("db change: %s -> %s (from %s)", key, values.newValue, values.oldValue);
         }
+        if (values.hasOwnProperty("newValue")) {
+          db[key] = values.newValue;
+        } else {
+          delete db[key];
+        }
+      }
     }
+  }
 });
 
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        let tabId = sender.tab.id;
-        if (DEBUG) console.log("request: %s tabid: %d", request, tabId);
-        if (request.action === "getEmail") {
-            let email = getEmail(request.domain);
+  (request, sender, sendResponse) => {
+    let tabId = sender.tab.id;
+    if (DEBUG) console.log("request: %s tabid: %d", request, tabId);
+    if (request.action === "getEmail") {
+      let email = getEmail(request.domain);
 
-            // Update icon state so page action gets enabled.
-            setPageState(tabId, request.domain, email, 1);
+      // Update icon state so page action gets enabled.
+      setPageState(tabId, request.domain, email, 1);
 
-            if (email) {
-                if (DEBUG) console.log("message-getEmail: %s -> %s", request.domain, email);
-                sendResponse({email: email});
-            }
-            else
-            {
-                if (DEBUG) console.log("message-getEmail: %s not found", request.domain);
-            }
-        }
-        else if (request.action === "setEmail") {
-            if (DEBUG) console.log("message-setEmail: %s -> %s", request.domain, request.email);
-            accountSelected(tabId, request.domain, request.email);
-            sendResponse({status: "registered"});
-        }
-        else {
-            console.log("message-unknown: ", request);
-        }
-        return true;
+      if (email) {
+        if (DEBUG) console.log("message-getEmail: %s -> %s", request.domain, email);
+        sendResponse({email: email});
+      } else {
+        if (DEBUG) console.log("message-getEmail: %s not found", request.domain);
+      }
+    } else if (request.action === "setEmail") {
+      if (DEBUG) console.log("message-setEmail: %s -> %s", request.domain, request.email);
+      accountSelected(tabId, request.domain, request.email);
+      sendResponse({status: "registered"});
+    } else {
+      console.log("message-unknown: ", request);
     }
+    return true;
+  }
 );
